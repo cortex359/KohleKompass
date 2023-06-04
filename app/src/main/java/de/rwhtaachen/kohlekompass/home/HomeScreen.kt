ef3a40ac358db70fc668c01110a43f9bf2c904d5
Julius Gummersbach <gummersbach@se-rwth.de>
2023-06-04 16:48:19 +0200

    optimize imports

--- app/src/main/java/de/rwhtaachen/kohlekompass/home/HomeScreen.kt
+++ app/src/main/java/de/rwhtaachen/kohlekompass/home/HomeScreen.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
