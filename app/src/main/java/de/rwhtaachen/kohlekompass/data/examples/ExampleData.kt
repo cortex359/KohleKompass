2d730fdb1a48eb51a6f1d97732b5b2c7174dbbe3
Julius Gummersbach <gummersbach@se-rwth.de>
2023-06-07 08:08:54 +0200

    change example name "Max" to "Paul" (max can be misleading in demo)

--- app/src/main/java/de/rwhtaachen/kohlekompass/data/examples/ExampleData.kt
+++ app/src/main/java/de/rwhtaachen/kohlekompass/data/examples/ExampleData.kt
@@ -15,35 +15,35 @@ val tagList = mutableStateListOf(
 )
 
 val userList = mutableStateListOf(
-    mutableStateOf(User("Max")),
+    mutableStateOf(User("Paul")),
     mutableStateOf(User("Julia")),
     mutableStateOf(User("Theo")),
     mutableStateOf(User("Laura")),
 )
 
 // currently loggid in user
-val meUser = User("Max")
+val meUser = User("Paul")
 
 val itemList = listOf(
-    ListItem("Lidl", "Max", "12,34€"),
+    ListItem("Lidl", "Paul", "12,34€"),
     ListItem("Aldi", "Laura", "5,60€"),
-    ListItem("DM", "Max", "8,56€"),
-    ListItem("Lieferando Pizza Mittwoch", "Max", "33,67€"),
+    ListItem("DM", "Paul", "8,56€"),
+    ListItem("Lieferando Pizza Mittwoch", "Paul", "33,67€"),
     ListItem("Aldi", "Laura", "23,45€"),
-    ListItem("Lidl", "Max", "12,34€"),
+    ListItem("Lidl", "Paul", "12,34€"),
     ListItem("Aldi", "Laura", "5,60€"),
-    ListItem("DM", "Max", "8,56€"),
-    ListItem("Lieferando Pizza Mittwoch", "Max", "33,67€"),
+    ListItem("DM", "Paul", "8,56€"),
+    ListItem("Lieferando Pizza Mittwoch", "Paul", "33,67€"),
     ListItem("Aldi", "Laura", "23,45€"),
-    ListItem("Lidl", "Max", "12,34€"),
+    ListItem("Lidl", "Paul", "12,34€"),
     ListItem("Aldi", "Laura", "5,60€"),
-    ListItem("DM", "Max", "8,56€"),
-    ListItem("Lieferando Pizza Mittwoch", "Max", "33,67€"),
+    ListItem("DM", "Paul", "8,56€"),
+    ListItem("Lieferando Pizza Mittwoch", "Paul", "33,67€"),
     ListItem("Aldi", "Laura", "23,45€"),
-    ListItem("Lidl", "Max", "12,34€"),
+    ListItem("Lidl", "Paul", "12,34€"),
     ListItem("Aldi", "Laura", "5,60€"),
-    ListItem("DM", "Max", "8,56€"),
-    ListItem("Lieferando Pizza Mittwoch", "Max", "33,67€"),
+    ListItem("DM", "Paul", "8,56€"),
+    ListItem("Lieferando Pizza Mittwoch", "Paul", "33,67€"),
     ListItem("Aldi", "Laura", "23,45€")
 )
 
